a8aa1dac26c5919d7b36beacb1a71805f95abe0a
Karthikeya Kollu <mail@karthikeyakollu.me>
2024-03-06 03:02:51 +0530

    added:upcommming eveents

--- src/components/home/UpcommingEvents.jsx
+++ src/components/home/UpcommingEvents.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { Card } from 'antd';
+import { Card, List, Avatar } from 'antd';
 export const UpcommingEvents = () => {
+  const data = [
+    {
+      title: 'Ant Design Title 1',
+    },
+    {
+      title: 'Ant Design Title 2',
+    },
+    {
+      title: 'Ant Design Title 3',
+    },
+    {
+      title: 'Ant Design Title 4',
+    },
+  ];
   return (
     <Card title="Card title" bordered={false} style={{ width: "100%" }}>
-      <p>Card</p>
-      <p>Card content</p>
-      <p>Card content</p>
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        renderItem={(item, index) => (
+          <List.Item>
+            <List.Item.Meta
+              avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+              title={<a href="https://ant.design">{item.title}</a>}
+              description="Ant Design, a refined"
+            />
+          </List.Item>
+        )}
+      />
     </Card>
   )
 }
